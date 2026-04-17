bdbc9a3426b137fdcfe2fab7c812d6d380bc2238
Chris Burton <heyburton415@gmail.com>
2026-04-16 22:01:36 -0700

    Made Calculated ET automatically save

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dial-In Pro Service Worker
 // Increment version when you update the app
-const VERSION = 'dialinpro-v3.4';
+const VERSION = 'dialinpro-v3.6';
 const CACHE = VERSION;
 
 const ASSETS = [
