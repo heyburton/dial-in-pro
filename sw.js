4616d001adedc0ca42a4521cc9ebef9b6606d0c9
Chris Burton <heyburton415@gmail.com>
2026-04-16 21:04:33 -0700

    Fixing a persistent weather data discrepancy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dial-In Pro Service Worker
 // Increment version when you update the app
-const VERSION = 'dialinpro-v3.2';
+const VERSION = 'dialinpro-v3.3';
 const CACHE = VERSION;
 
 const ASSETS = [
