8ebecdfae7b83076359022dcb8f4b2155ff8ec07
Chris Burton <heyburton415@gmail.com>
2026-04-17 19:32:07 -0700

    Updated csv file to includer all log data

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dial-In Pro Service Worker
 // Increment version when you update the app
-const VERSION = 'dialinpro-v4.9';
+const VERSION = 'dialinpro-v5.1';
 const CACHE = VERSION;
 
 const ASSETS = [
