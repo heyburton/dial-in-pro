4baa0457d9176430a0bfb875ef3ee94dfa9afed0
Chris Burton <heyburton415@gmail.com>
2026-04-17 21:42:31 -0700

    Pushed algorithm correction live

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dial-In Pro Service Worker
 // Increment version when you update the app
-const VERSION = 'dialinpro-v5.2';
+const VERSION = 'dialinpro-v5.3';
 const CACHE = VERSION;
 
 const ASSETS = [
