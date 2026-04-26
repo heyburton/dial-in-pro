d8b9f5072bba3caa815ba788bc9231c417a4cc1b
Chris Burton <heyburton415@gmail.com>
2026-04-26 16:00:34 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Dial-In Pro Service Worker
 // Increment version when you update the app
-const VERSION = 'dialinpro-v8.5';
+const VERSION = 'dialinpro-v8.6';
 const CACHE = VERSION;
 
 const ASSETS = [
